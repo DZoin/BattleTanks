af3704484fe459afa229287ff978e065ec600eb0
dimitard <dimitar.dimitrov@pcloud.com>
2016-10-24 23:54:31 +0300

    Game Class | Implemented Game class, which cointains the main game loop and the draw and update functions. Implemented Input class which holds maps of the pressed, released and help keys.

---
+++ BattleTanks/Game.cc
@@ -0,0 +1,72 @@
+#include <SDL.h>
+#include "Game.h"
+#include "Input.h"
+#include <algorithm>
+#include <string>
+using namespace std;
+/*	Game class
+ *	Holds all information about the main game loop
+ */
+namespace
+{
+	const int FPS = 50;
+	const int MAX_FRAME_TIME = 5 * 1000 / FPS;
+}
+
+Game::Game()
+{
+	SDL_Init(SDL_INIT_EVERYTHING);
+	gameLoop();
+}
+Game::~Game()
+{
+
+}
+void Game::gameLoop()
+{
+	Graphics graphics("Battle Tanks", 800, 600);
+	Input input;
+	SDL_Event event;
+
+	int LAST_UPDATE_TIME = SDL_GetTicks();
+
+	while (true)
+	{
+		input.beginNewFrame();
+		if (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_KEYDOWN)
+			{
+				if (event.key.repeat == 0)
+				{
+					input.keyDownEvent(event);
+				}
+			} 
+			else if (event.type == SDL_KEYUP)
+			{
+				input.keyUpEvent(event);
+			}
+			else if (event.type == SDL_QUIT)
+			{
+				return;
+			} 
+		}
+		if (input.wasKeyPressed(SDL_SCANCODE_ESCAPE) == true)
+		{
+			return;
+		}
+		const int CURRENT_TIME_MS = SDL_GetTicks();
+		int ELAPSED_TIME_MS = CURRENT_TIME_MS - LAST_UPDATE_TIME;
+		update(std::min(ELAPSED_TIME_MS, MAX_FRAME_TIME));
+
+		LAST_UPDATE_TIME = CURRENT_TIME_MS;
+	}
+}
+void Game::draw(Graphics &graphics)
+{
+
+}
+void Game::update(float elapsedTime)
+{
+
+}
