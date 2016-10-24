8d04d1e32aed2d0219c1398433bb8ef91bee2768
Dimitar Zoin <dimitar.zoin@gmail.com>
2016-10-24 21:50:04 +0300

    Added Actor and Tank classes

--- BattleTanks/Actor.h
+++ BattleTanks/Actor.h
@@ -1,10 +1,15 @@
 #pragma once
+
 class Actor
 {
 public:
 	Actor();
+	Actor(unsigned x_coord, unsigned y_coord);
 	virtual ~Actor() = 0;
 	virtual void update() = 0;
 	virtual void draw() = 0;
+
+protected:
+	unsigned int _x_coord, _y_coord;
 };
 
